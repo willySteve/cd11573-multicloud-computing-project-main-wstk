20a719594ddaf1806599604c646812a46a713280
willySteve <steeve.tchouatcha.dev21@gmail.com>
2023-03-08 17:25:04 +0100

    updated the dependecies of azures

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -42,24 +42,7 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
 
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
-}
 
 # Azure sql
 provider "azurerm" {
