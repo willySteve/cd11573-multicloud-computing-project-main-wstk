8ed858232a0c79aacc567ad0d8d315b8a9ab3758
willySteve <steeve.tchouatcha.dev21@gmail.com>
2023-03-08 17:31:33 +0100

    updated dynamodb table

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -30,20 +30,6 @@ resource "azurerm_container_group" "udacity" {
 }
 
 ####### Your Additions Will Start Here ######
-# Terraform module
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
-
-
 # Azure sql
 provider "azurerm" {
   features {}
