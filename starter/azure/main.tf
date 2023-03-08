3c9ef128c24de2c68a0f310c59b2c4e96ffee73f
willySteve <steeve.tchouatcha.dev21@gmail.com>
2023-03-08 16:21:30 +0100

    first version of the project

---
+++ starter/azure/main.tf
@@ -0,0 +1,111 @@
+data "azurerm_resource_group" "udacity" {
+  name     = "Regroup_4gKqrgD_cn"
+}
+
+resource "azurerm_container_group" "udacity" {
+  name                = "udacity-continst"
+  location            = data.azurerm_resource_group.udacity.location
+  resource_group_name = data.azurerm_resource_group.udacity.name
+  ip_address_type     = "Public"
+  dns_name_label      = "udacity-willy-azure"
+  os_type             = "Linux"
+
+  container {
+    name   = "azure-container-app"
+    image  = "docker.io/tscotto5/azure_app:1.0"
+    cpu    = "0.5"
+    memory = "1.5"
+    environment_variables = {
+      "AWS_S3_BUCKET"       = "udacity-willy-aws-s3-bucket",
+      "AWS_DYNAMO_INSTANCE" = "udacity-willy-aws-dynamodb"
+    }
+    ports {
+      port     = 3000
+      protocol = "TCP"
+    }
+  }
+  tags = {
+    environment = "udacity"
+  }
+}
+
+####### Your Additions Will Start Here ######
+# Terraform module
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Azure sql
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplesa"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_mssql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "test" {
+  name           = "acctest-db-d"
+  server_id      = azurerm_mssql_server.example.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+
+  tags = {
+    foo = "bar"
+  }
+}
+
+# Dotnet web app
+resource "azurerm_static_site" "example" {
+  name                = "example"
+  resource_group_name = "example"
+  location            = "West Europe"
+}
