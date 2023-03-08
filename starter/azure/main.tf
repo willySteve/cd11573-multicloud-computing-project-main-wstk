931eb767cfa2619ec51f63e175ca4701e9c00f2d
willySteve <steeve.tchouatcha.dev21@gmail.com>
2023-03-08 17:54:04 +0100

    changed group name and location

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -33,16 +33,16 @@ resource "azurerm_container_group" "udacity" {
 # Azure sql
 resource "azurerm_storage_account" "example" {
   name                     = "examplesa"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = data.azurerm_resource_group.udacity.name
+  location                 = data.azurerm_resource_group.udacity.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_mssql_server" "example" {
   name                         = "example-sqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
+  resource_group_name          = data.azurerm_resource_group.udacity.name
+  location                     = data.azurerm_resource_group.udacity.location
   version                      = "12.0"
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
